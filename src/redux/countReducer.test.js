bec670049ac7fb9f5824422900e821e6ef7fc4de
Jakub Walczak <jakub.s.walczak@gmail.com>
2018-12-19 10:30:09 +0100

    Add Redux boilerplate with skipped tests

---
+++ src/redux/countReducer.test.js
@@ -0,0 +1,16 @@
+// import countReducer from "./countReducer";
+// import { incrementCount } from "./actionCreators";
+
+xdescribe("countReducer", () => {
+  it("provides initial state", () => {
+    expect(countReducer()).toBe(0);
+  });
+
+  it("handles INCREMENT_COUNT action", () => {
+    const action = incrementCount();
+    const initialState = 10;
+    const state = countReducer(initialState, action);
+
+    expect(state).toBe(11);
+  });
+});
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+const initialState = {
+  savedNumbers: [],
+  count: 0,
+  countInput: ""
+};
